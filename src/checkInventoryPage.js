4c1385ce808d4554f6e21ced5cdbfcf854bc0f73
hoholyin <holyin97@gmail.com>
2021-08-09 20:32:40 +0800

    add refresh feature

--- src/checkInventoryPage.js
+++ src/checkInventoryPage.js
@@ -2,10 +2,11 @@ import logo from "./assets/logo_transparent.png";
 import InventoryList from "./InventoryList";
 import {apiEndpoint} from "./common";
 import {getRequest} from "./requestBuilder";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import Loader from "./Loader";
 import "./checkInventoryPage.css"
 import back from "./assets/back.png";
+import refresh from "./assets/refresh.png";
 
 const CheckInventoryPage = (props) => {
     const [inventoryList, setInventoryList] = useState([]);
@@ -13,24 +14,7 @@ const CheckInventoryPage = (props) => {
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-        async function retrieveInventories() {
-            setIsLoading(true)
-            const inventoryListObject = await getRequest(apiEndpoint + '/inventory')
-            const allInventories = inventoryListObject.data.allInventories
-            allInventories.map((e) => {
-                e.name = e.code
-                e.name = e.name.replaceAll("NA", "")
-                e.name = e.name.replaceAll("Phone accessories", "")
-                e.name = e.name.replaceAll("Airpods case", "")
-                e.name = e.name.replaceAll("Phone case", "")
-                return e
-            })
-            setAllInventories(allInventories)
-            setInventoryList(allInventories)
-            setIsLoading(false)
-        }
-
-        retrieveInventories();
+        refreshInventory();
     }, [])
 
     const search = (searchQuery) => {
@@ -47,13 +31,35 @@ const CheckInventoryPage = (props) => {
         setInventoryList(filteredItems)
     }
 
+    const refreshInventory = async () => {
+        setIsLoading(true)
+        const inventoryListObject = await getRequest(apiEndpoint + '/inventory')
+        const allInventories = inventoryListObject.data.allInventories
+        allInventories.map((e) => {
+            e.name = e.code
+            e.name = e.name.replaceAll("NA", "")
+            e.name = e.name.replaceAll("Phone accessories", "")
+            e.name = e.name.replaceAll("Airpods case", "")
+            e.name = e.name.replaceAll("Phone case", "")
+            return e
+        })
+        setAllInventories(allInventories)
+        setInventoryList(allInventories)
+        setIsLoading(false)
+    }
+
     return (
         <div className="App">
             <header className="App-header">
-                <div className="back-button-container" onClick={() => props.navigate("")}>
-                    <img src={back} className="back-button" alt="back"/>
+                <div className="button-containers">
+                    <div className="back-button-container" onClick={() => props.navigate("")}>
+                        <img src={back} className="back-button interactive" alt="back"/>
+                    </div>
+                    <div className="refresh-button-container" onClick={refreshInventory}>
+                        <img src={refresh} className="refresh-button interactive" alt="refresh"/>
+                    </div>
                 </div>
-                <div className="logo-container" onClick={() => props.navigate("")}>
+                <div className="logo-container interactive" onClick={() => props.navigate("")}>
                     <img src={logo} className="submit-order-app-logo" alt="logo"/>
                 </div>
                 <input className="input-box" type="text" onChange={e => search(e.target.value)}/>
